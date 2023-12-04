a2137fb480129a7997c75f4e792cfcecc461435d
Kobe Middleton <kobe.middleton62@gmail.com>
2023-12-03 23:59:53 -0500

    added correct subcommand stuff

--- src/main/java/formato/middleton/CLI.java
+++ src/main/java/formato/middleton/CLI.java
@@ -3,6 +3,104 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
+@Command(name= "CLI", subcommands = {create.class, read.class, update.class, delete.class})
+public class CLI 
+{}
+
+@Command(name="create", description= "Create a record on the database")
+class create implements Runnable 
+{
+    @Parameters(paramLabel = "<name>", 
+               description = "The name of the event.")
+    private String event_name;
+
+    @Parameters(paramLabel = "<prize_pool_amount>", 
+               description = "Amount of money in prize pool for the event")
+    private String prize_pool;
+
+    @Override
+    public void run() 
+    {
+        ClientService client = new ClientService();
+        try 
+        {
+            client.create(event_name, prize_pool);
+        }catch(Exception e) {
+            System.out.println(e);
+        }
+    }
+}
+
+@Command(name="read", description= "Read a record on the database")
+class read implements Runnable 
+{
+    @Parameters(paramLabel = "<id number for record>", 
+               description = "Used to differentiate records in the database.")
+    private int id;
+
+    @Override
+    public void run() 
+    {
+        ClientService client = new ClientService();
+        try 
+        {
+            client.read(id);
+        }catch(Exception e) {
+            System.out.println(e);
+        }
+    }
+}
+
+@Command(name="update", description= "Update a record on the database")
+class update implements Runnable 
+{
+    @Parameters(paramLabel = "<name>", 
+               description = "The name of the event.")
+    private String event_name;
+
+    @Parameters(paramLabel = "<prize_pool_amount>", 
+               description = "Amount of money in prize pool for the event")
+    private String prize_pool;
+
+    @Parameters(paramLabel = "<id number for record>", 
+               description = "Used to differentiate records in the database.")
+    private int id;
+
+    @Override
+    public void run() 
+    {
+        ClientService client = new ClientService();
+        try 
+        {
+            client.update(id, event_name, prize_pool);
+        }catch(Exception e) {
+            System.out.println(e);
+        }
+    }
+
+}
+
+@Command(name="delete", description= "Delete a record on the database")
+class delete implements Runnable 
+{
+    @Parameters(paramLabel = "<id number for record>", 
+               description = "Used to differentiate records in the database.")
+    private int id;
+
+    @Override
+    public void run() 
+    {
+        ClientService client = new ClientService();
+        try 
+        {
+            client.delete(id);
+        }catch(Exception e) {
+            System.out.println(e);
+        }
+    }
+
+}
+
 @Command(name = "CLI", mixinStandardHelpOptions = true) 
 public class CLI implements Runnable
 {
