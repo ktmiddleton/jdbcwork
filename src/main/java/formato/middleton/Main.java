117eabb7a9a274b769e5b1412ce1617d260c8736
Jack Formato <jpformato@loyola.edu>
2023-12-04 00:18:46 -0500

    script added

--- src/main/java/formato/middleton/Main.java
+++ src/main/java/formato/middleton/Main.java
@@ -1,13 +1,15 @@
 package formato.middleton;
-import cli.java;
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 public class Main {
-    public static void main(String[] args) 
+    public static void main(String[] args)
     {
-        int exitCode = new CommandLine(new CLI()).execute(args); 
+        int exitCode = new picocli.CommandLine(new CLI()).execute(args);
         System.exit(exitCode);
     }
 }
