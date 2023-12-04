393d16fe7a1230525e21ddc5b0258fe2929bff82
Jack Formato <jpformato@loyola.edu>
2023-12-03 21:36:24 -0500

    DAO started

---
+++ src/main/java/formato/middleton/EventDAO.java
@@ -0,0 +1,27 @@
+package formato.middleton;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EventDAO extends AbstractDAO<Event> {
+    public void create(Event entity) throws SQLException {
+
+    }
+
+    public Event read(int id) throws SQLException {
+
+    }
+
+    public void update(Event entity) throws SQLException {
+
+    }
+
+    public void delete(Event entity) throws SQLException {
+
+    }
+
+    public List<Event> list() throws SQLException {
+
+    }
+}
