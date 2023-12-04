423e088e93e5de331014a700b8adb2b49a8a4934
Jack Formato <jpformato@loyola.edu>
2023-12-03 21:21:06 -0500

    Event class

---
+++ src/main/java/formato/middleton/Event.java
@@ -0,0 +1,40 @@
+package formato.middleton;
+
+public class Event {
+    private int id;
+    private String name;
+    private int prizePool;
+
+    public Event() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPrizePool() {
+        return prizePool;
+    }
+
+    public void setPrizePool(Integer prizePool) {
+        this.prizePool = prizePool;
+    }
+
+    @Override
+    public String toString() {
+        return Integer.toString(id) + " | " + name + ": " + Integer.toString(prizePool);
+    }
+}
+
